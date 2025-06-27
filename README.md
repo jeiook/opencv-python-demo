2559987c2f511bc97a4722dce60b0f0d5c220734
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2025-06-27 12:59:10 -0700

    Refactor for multiple models

---
+++ README.md
@@ -0,0 +1,15 @@
+# opencv-python-demo
+
+Playground for testing the opencv python library.
+
+Currently, text detection is the focus of the project.
+
+## Setup
+
+Install the dependencies via `pip install`.
+
+You will need to download the text detection models and put them in the `models/` directory (you may need to adjust the file names in the code to match the actual file name).
+
+You will also need download your own images and put them in the `images/` directory (again, adjusting file names as necessary).
+
+Then run the code via `python3 main.py`.
--- main.py
+++ main.py
@@ -1,32 +1,65 @@
 import cv2
 import numpy as np
 
-IMAGE_PATHS = ["Example_Image_1.jpg", "Example_Image_2-1024x681.jpg", "Example_Image_3-1024x512.png", "Example_Image_4.png"]
-MODEL_PATH = "frozen_east_text_detection.pb"
+IMAGE_PATHS = ["images/" + path for path in [
+    "Example_Image_1.jpg", 
+    "Example_Image_2-1024x681.jpg", 
+    "Example_Image_3-1024x512.png", 
+    "Example_Image_4.png", 
+    "Example_Receipt_Image.jpeg"]
+]
+MODEL_PATHS = {key: "models/" + value for key, value in ({
+    "EAST": "frozen_east_text_detection.pb",
+    "DB50": "DB_TD500_resnet50.onnx"
+}).items()}
 DEFAULT_SCALE = 1.0
 DEFAULT_MEAN = (122.67891434, 116.66876762, 104.00698793)
 INPUT_SIZE = (320, 320)
 
-def imagePathToImage(imagePath: str, inputSize: tuple[int, int]):
+def getImageFromFilePath(imagePath: str, inputSize: tuple[int, int]):
     return cv2.resize(cv2.imread(imagePath), inputSize)
 
-def detectText(origImage, confThresh: float, nmsThresh: float, inputSize: tuple[int, int], mean: tuple[float, float, float]):
-    textDetectorEAST= cv2.dnn_TextDetectionModel_EAST(MODEL_PATH) # type: ignore
-    textDetectorEAST.setConfidenceThreshold(confThresh)
-    textDetectorEAST.setNMSThreshold(nmsThresh)
-    textDetectorEAST.setInputParams(DEFAULT_SCALE, inputSize, mean, True)
-    boxes, confidences = textDetectorEAST.detect(origImage)
+def configureEASTTextDetector(textDetector, confThresh: float, nmsThresh: float, inputSize: tuple[int, int], mean: tuple[float, float, float]) -> None:
+    textDetector.setConfidenceThreshold(confThresh)
+    textDetector.setNMSThreshold(nmsThresh)
+    textDetector.setInputParams(DEFAULT_SCALE, inputSize, mean, True)
+
+def configureDBTextDetector(textDetector, binThresh: float, polyThresh: float, inputSize: tuple[int, int], mean: tuple[float, float, float]) -> None:
+    textDetector.setBinaryThreshold(binThresh)
+    textDetector.setPolygonThreshold(polyThresh)
+    textDetector.setInputParams(DEFAULT_SCALE/255, inputSize, mean, True)
+
+def detectText(origImage, textDetector):
+    boxes, confidences = textDetector.detect(origImage)
     return boxes
 
 def main():
-    origImage = imagePathToImage(IMAGE_PATHS[3], INPUT_SIZE)
-    annotated_image = origImage.copy()
-    boxes = detectText(origImage, 0.8, 0.4, INPUT_SIZE, DEFAULT_MEAN)
-    for box in boxes:
-        cv2.polylines(annotated_image, [np.array(box, np.int32)], isClosed=True, color=(0, 255, 255), thickness=1)  # Annotate EAST (Cyan)
+    origImage = getImageFromFilePath(IMAGE_PATHS[-1], INPUT_SIZE)
+    runs = [
+        {
+            "name": "EAST",
+            "textDetector": cv2.dnn_TextDetectionModel_EAST(MODEL_PATHS["EAST"]), # type: ignore
+            "configureTextDetector": lambda textDetector: configureEASTTextDetector(textDetector, 0.8, 0.4, INPUT_SIZE, DEFAULT_MEAN),
+            "colorTuple": (0, 255, 255)
+        }, 
+        {
+            "name": "DB50",
+            "textDetector": cv2.dnn_TextDetectionModel_DB(MODEL_PATHS["DB50"]), # type: ignore
+            "configureTextDetector": lambda textDetector: configureDBTextDetector(textDetector, 0.3, 0.5, INPUT_SIZE, DEFAULT_MEAN),
+            "colorTuple": (0, 0, 255)
+        }
+    ]
+    
+    annotatedImages = [origImage.copy() for _ in runs]
+    for index, run in enumerate(runs):
+        run["configureTextDetector"](run["textDetector"])
+        boxes = detectText(origImage, run["textDetector"])
+        for box in boxes:
+            cv2.polylines(annotatedImages[index], [np.array(box, np.int32)], isClosed=True, color=run["colorTuple"], thickness=1)
 
     cv2.imshow('Original', origImage)
-    cv2.imshow('Annotated', annotated_image)
+    for index, run in enumerate(runs):
+        cv2.imshow(f'Annotated {run["name"]}', annotatedImages[index])
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
