0d072feed15544fdebaf04adcb1bc29080bbaf98
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2025-06-27 16:15:42 -0700

    Added DB18 model

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ IMAGE_PATHS = ["images/" + path for path in [
 ]
 MODEL_PATHS = {key: "models/" + value for key, value in ({
     "EAST": "frozen_east_text_detection.pb",
-    "DB50": "DB_TD500_resnet50.onnx"
+    "DB50": "DB_TD500_resnet50.onnx",
+    "DB18": "DB_TD500_resnet18.onnx"
 }).items()}
 DEFAULT_SCALE = 1.0
 DEFAULT_MEAN = (122.67891434, 116.66876762, 104.00698793)
@@ -40,13 +41,19 @@ def main():
             "name": "EAST",
             "textDetector": cv2.dnn_TextDetectionModel_EAST(MODEL_PATHS["EAST"]), # type: ignore
             "configureTextDetector": lambda textDetector: configureEASTTextDetector(textDetector, 0.8, 0.4, INPUT_SIZE, DEFAULT_MEAN),
-            "colorTuple": (0, 255, 255)
+            "colorTuple": (0, 255, 255) # cyan
         }, 
         {
             "name": "DB50",
             "textDetector": cv2.dnn_TextDetectionModel_DB(MODEL_PATHS["DB50"]), # type: ignore
             "configureTextDetector": lambda textDetector: configureDBTextDetector(textDetector, 0.3, 0.5, INPUT_SIZE, DEFAULT_MEAN),
-            "colorTuple": (0, 0, 255)
+            "colorTuple": (0, 0, 255) # red
+        },
+        {
+            "name": "DB18",
+            "textDetector": cv2.dnn_TextDetectionModel_DB(MODEL_PATHS["DB18"]), # type: ignore
+            "configureTextDetector": lambda textDetector: configureDBTextDetector(textDetector, 0.3, 0.5, INPUT_SIZE, DEFAULT_MEAN),
+            "colorTuple": (255, 0, 0) # blue
         }
     ]
     
